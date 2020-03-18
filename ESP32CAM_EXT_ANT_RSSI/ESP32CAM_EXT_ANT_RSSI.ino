ffb4d47f1b2147bb4abd8172a33c750825e1dbbf
AcoptexCom <58400137+AcoptexCom@users.noreply.github.com>
2020-03-18 21:53:20 +0200

    Add files via upload

---
+++ ESP32CAM_EXT_ANT_RSSI/ESP32CAM_EXT_ANT_RSSI.ino
@@ -0,0 +1,21 @@
+#include "WiFi.h"
+
+const char* ssid     = "TR-2";
+const char* password = "1234";
+
+void setup(){
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected.");
+  delay(100);
+}
+
+void loop(){
+  Serial.print("RSSI: ");
+  Serial.println(WiFi.RSSI());
+  delay(1000);
+}
